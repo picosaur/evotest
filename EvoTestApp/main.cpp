d3de155a54c0fbfa2701f04423755bda77705c22
Pico Saur <picosaur@tuta.io>
2026-01-16 18:45:15 +0300

    gix deploy

--- EvoTestApp/main.cpp
+++ EvoTestApp/main.cpp
@@ -7,8 +7,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
+    /*
     QTranslator translator;
+
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
         const QString baseName = "EvoTestApp_" + QLocale(locale).name();
@@ -16,7 +17,7 @@ int main(int argc, char *argv[])
             a.installTranslator(&translator);
             break;
         }
-    }
+    }*/
     MainWindow w;
     w.show();
     return a.exec();
